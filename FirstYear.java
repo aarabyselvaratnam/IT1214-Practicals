ae231ab01002bcee9cdc862290c71bd35b6ca2fe
aarabyselvaratnam <aarabys2000@gmail.com>
2025-04-13 14:38:41 +0530

    Add files via upload

--- FirstYear.java
+++ FirstYear.java
@@ -1,7 +1,7 @@
-package AMC;
+package IT;
 
 public class FirstYear{
 	public void display(){
-		System.out.println("Hi from FirstYear in Package AMC");
+		System.out.println("Hi from FirstYear in Package IT");
 	}
 }
