e1e4a2b91ca3b90f8b7f4a80b1f86ad28b254872
aarabyselvaratnam <aarabys2000@gmail.com>
2025-04-14 11:18:05 +0530

    Add files via upload

---
+++ S3/Invoice/App.java
@@ -0,0 +1,9 @@
+import Human.* ;
+
+class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Invoice App");
+		Person aaraby = new Person ("Aaraby") ;
+		aaraby.talk("Hello from Aaraby") ;
+	}
+}
